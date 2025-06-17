e310165aa3a32f3a3dca0e96f4eaf6e3eb5ebfc7
wkhamcha <68030259@kmitl.ac.th>
2025-06-17 03:31:54 +0000

    ex

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "sunjita" 
+Iast_name = "wichajarn"
+print(first_name,Iast_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "sunjita"
+Iast_name = "wichajarn"
+whoIe_name = first_name + " " + Iast_name
+print(whoIe_mame)
